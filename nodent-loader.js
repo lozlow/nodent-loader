8fb97b621a12daff1d79fdc1c76c8ef051f59204
Pete Shaw <pete.shaw@mailonline.co.uk>
2016-03-21 10:30:14 +0000

    nodent-loader now correctly reports errors encountered by nodent

--- nodent-loader.js
+++ nodent-loader.js
@@ -6,6 +6,11 @@ const nodent = require('nodent')();
 module.exports = function nodentCompile(content, sourceMap) {
 	const callback = this.async();
 	const params = loaderUtil.parseQuery(this.query);
-	const compiled = nodent.compile(content, this.resourcePath, sourceMap, Object.assign({ sourcemap: !!this._compiler.devtool }, params));
-	callback(null, compiled.code, compiled.sourcemap);
+	try {
+		const compiled = nodent.compile(content, this.resourcePath, sourceMap, Object.assign({ sourcemap: !!this._compiler.devtool }, params));
+		callback(null, compiled.code, compiled.sourcemap);
+	} catch (ex) {
+		console.log('exception in nodent-loader');
+		callback(ex);
+	}
 };
